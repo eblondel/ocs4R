21e4f79800f2c68022945c3c992e77f5e2a0a349
eblondel <emmanuel.blondel1@gmail.com>
2019-11-19 08:12:04 +0100

    test CI nextcloud autoconfig set-up

---
+++ tests/resources/nextcloud/autoconfig.php
@@ -0,0 +1,9 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "sqlite",
+  "dbname"        => "nextcloud",
+  "dbtableprefix" => "",
+  "adminlogin"    => "admin",
+  "adminpass"     => "admin",
+  "directory"     => "/www/htdocs/nextcloud/data",
+);
