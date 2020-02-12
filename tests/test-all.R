9019b3d3b5aa83999b2bd3c713cec87a1b5c42e9
eblondel <emmanuel.blondel1@gmail.com>
2020-02-12 13:27:15 +0100

    run CI on travis only

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,21 +1,25 @@
 library(testthat)
 library(ocs4R)
 
-#test environment
-ocs_url <- "http://localhost:8080"
-ocs_user <- "admin"
-ocs_pwd <- "admin"
+if(Sys.getenv("NOT_CRAN") == "true"){
 
-OCS <- ocsManager$new(
-  url = "http://localhost:8080", 
-  user = ocs_user, 
-  pwd = ocs_pwd, 
-  logger = "DEBUG"
-)
+  #test environment
+  ocs_url <- "http://localhost:8080"
+  ocs_user <- "admin"
+  ocs_pwd <- "admin"
+  
+  OCS <- ocsManager$new(
+    url = "http://localhost:8080", 
+    user = ocs_user, 
+    pwd = ocs_pwd, 
+    logger = "DEBUG"
+  )
+  
+  if(is(OCS, "ocsManager")){
+    cat(sprintf("OCS REST endpoint at '%s' configured with token. Running integration tests...\n", ocs_url))
+    test_check("ocs4R")
+  }else{
+    cat("OCS REST endpoint at '%s' not configured. Skipping integration tests...\n")
+  }
 
-if(is(OCS, "ocsManager")){
-  cat(sprintf("OCS REST endpoint at '%s' configured with token. Running integration tests...\n", ocs_url))
-  test_check("ocs4R")
-}else{
-  cat("OCS REST endpoint at '%s' not configured. Skipping integration tests...\n")
 }
